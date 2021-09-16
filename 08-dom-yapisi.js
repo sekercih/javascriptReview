f893dd74586ac831802a8b75ca7d12eb94cc95b8
sekercih <hkkskrc@gmail.com>
2021-09-16 14:53:39 +0300

    dom vs

---
+++ 08-dom-yapisi.js
@@ -0,0 +1,8 @@
+console.log(document.head)
+console.log(document.body);
+
+console.log(document.URL);
+
+console.log(document.TEXT_NODE);
+document.body.style.backgroundColor="red"
+document.head.style.backgroundColor="red"
---
+++ 09-dom-elemanlari.js
@@ -0,0 +1,24 @@
+let h2 = document.getElementById("title");
+
+console.log((h2.innerHTML = "Deneme"));
+
+let link = document.querySelector("ul");
+
+console.log(link);
+
+const dil = null;
+
+const sayi1 = 1;
+const sayi2 = "1";
+console.log(sayi1 + sayi2);
+
+const ad = "Mehmet";
+const yas = 25;
+console.log(`Benim adim: ${ad} yaşım ${yas}`);
+
+const dogum = 1991;
+const yil = 2021;
+const yas1 = yil - dogum;
+console.log(yas1);
+
+console.log(`Yaş ${yil-dogum}`);
